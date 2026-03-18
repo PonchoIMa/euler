f77492141d0f37702137a52b9cba0089757f35ad
Alfonso Izaguirre <alfonso.izaguirre@tuta.io>
2026-03-17 19:38:55 -0600

    Update logic on pset005.py to be O(k log log k)!

--- pset005.py
+++ pset005.py
@@ -2,27 +2,44 @@
 # What is the smallest positive number that is evenly divisible, divisible with no remainder by all of the numbers from to 20?
 import argparse, logging
 
-def least_common_multiple(candidate: int, n: int) -> int:
-    # if a number is not even, return 0
-    if(candidate % 2 != 0):
-        return 0
+def find_primes_up_to(n: int) -> list:
+    logging.debug(f'Finding primes up to {n}!')
+    primes = []
+    
+    if(n > 2):
+        primes = [2]
 
-    # check for all cases until n
-    for i in range(3, n):
-        if(candidate % i != 0):
-            return 0
+        for candidate in range(3, n, 2):
+            is_prime = 1 
 
-    # a last test, in case n % 2 == 0
-    if(candidate % n != 0):
-        return 0
+            for prime in primes:
+                if(candidate % prime == 0):
+                    is_prime = 0
+                    break
 
-    logging.debug(f'A new candidate has been found: {candidate}!')
-    return 1
+            if(is_prime):
+                primes.append(candidate)
 
-def factorial(n: int) -> int:
-    if n != 1:
-        return n * factorial(n - 1)
-    return 1
+    logging.debug(f'Primes were: {primes}')
+    return primes
+
+def find_lcm(n: int) -> int:
+    result = 1
+    primes = find_primes_up_to(n)
+
+    logging.debug(f'Beginning process to find the highest prime factor for the primes found')
+
+    for prime in primes:
+        highest_prime_factor = 1
+        while(1):
+            if(prime ** highest_prime_factor > n):
+                logging.debug(f'Highest prime factor found {prime} ** {highest_prime_factor - 1} = {prime ** (highest_prime_factor - 1)}')
+                result *= (prime ** (highest_prime_factor - 1))
+                break
+            highest_prime_factor += 1
+
+    return result
+
 
 def main(n: int, verbose: bool):
     if(verbose):
@@ -31,18 +48,8 @@ def main(n: int, verbose: bool):
         logging.basicConfig(level=logging.WARNING) # Only show errors by default
 
     # finding the limit to search, which is n!
-    current_find = factorial(n)
-    logging.debug(f'Search limit has been set to {current_find}!')
-
-    for candidate in range(n ** 2, current_find, 2):
-        if(least_common_multiple(candidate, n)):
-            logging.debug(f'Replacing {current_find} -> {candidate}!')
-            current_find = candidate
-            break
-
-    # apparently, this is the one?
-    logging.debug(f'Least common multiple is {current_find}!')
-    print(current_find)
+    logging.debug(f'Search limit has been set to {n}!')
+    print(find_lcm(n))
     return 0
 
 if(__name__ == '__main__'):
